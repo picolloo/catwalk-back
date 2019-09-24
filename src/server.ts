7a9f4c2aef3d60711706070771902e74cd92a2c5
picolloo <lucaspicolloo@gmail.com>
2019-09-24 14:07:04 -0300

    nodemon watching ts files

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,28 @@
 import * as Koa from 'koa';
+import * as Router from 'koa-router';
+
+import * as logger from 'koa-logger';
+import * as json from 'koa-json';
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx, next) => {
-  ctx.body = 'Hello TS';
+router.get('/', async (ctx, next) => {
+  ctx.body = { msg: 'Hello TS' };
 
   await next();
 });
+
+router.get('/about', async (ctx, next) => {
+  ctx.body = { msg: 'Hello about' };
+
+  await next();
+});
+
+// Middlewares
+app.use(logger());
+app.use(json());
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000, () => console.log('Server bound to port 3000'));
