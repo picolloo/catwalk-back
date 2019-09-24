10b4d4ae81e3e6691a5fe856d708cc52bda69a43
picolloo <lucaspicolloo@gmail.com>
2019-09-24 11:33:03 -0300

    dist removed from remote

--- dist/server.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var _koa = require('koa');
-
-var _koa2 = _interopRequireDefault(_koa);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
