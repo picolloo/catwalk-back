fca8d0359c23d606de9fa811e677f7e8f70c27d2
picolloo <lucaspicolloo@gmail.com>
2019-09-24 11:28:33 -0300

    npm scripts with babel implemented

---
+++ dist/server.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var _koa = require('koa');
+
+var _koa2 = _interopRequireDefault(_koa);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
