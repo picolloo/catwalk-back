11ec58ea06477ccccc1e388f9d5554eddf4ea133
picolloo <lucaspicolloo@gmail.com>
2019-09-24 21:14:48 -0300

    Database connection refactor

--- src/database/index.js
+++ src/database/index.js
@@ -1,39 +1,12 @@
-import fs from 'fs';
-import path from 'path';
+import mongoose from "mongoose";
 
-import mongoose from 'mongoose';
-
-/**
- *  Creates the Mongoose model from Model directory files
- */
-const createMongoModel = async (modelName, modelDir) => {
-  const model = await import(path.resolve(modelDir, modelName));
-
-  const schema = new mongoose.Schema(model);
-
-  return mongoose.model(modelName, schema);
-};
-
-const initDatabase = async (modelsDir, databaseUri) => {
+export default async databaseUri => {
   try {
-    mongoose.connect(databaseUri, { useNewUrlParser: true });
-
-    const db = {};
-
-    const files = fs.readdirSync(modelsDir);
-
-    files.forEach(async file => {
-      const fileName = file.replace(/\.js$/, '');
-
-      db[fileName] = await createMongoModel(fileName, modelsDir);
-
-      console.info(`Model being loaded: ${fileName}`);
+    mongoose.connect(databaseUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     });
-
-    return Promise.resolve(db);
-  } catch (e) {
-    return Promise.reject(e);
+  } catch (err) {
+    throw new Error("Error trying to connect to MongoDB");
   }
 };
-
-export default initDatabase;
--- src/server.js
+++ src/server.js
@@ -1,32 +1,24 @@
-import Koa from 'koa';
-import dotenv from 'dotenv';
+import Koa from "koa";
+import dotenv from "dotenv";
 dotenv.config();
 
-import logger from 'koa-logger';
-import json from 'koa-json';
-import bodyParser from 'koa-bodyparser';
+import logger from "koa-logger";
+import json from "koa-json";
+import bodyParser from "koa-bodyparser";
 
-import router from './routes';
-import initDatabase from './database';
+import router from "./routes";
+import initDatabase from "./database";
+
+initDatabase(process.env.DATABASE_URI);
 
 const app = new Koa();
 
-// app.use(async ctx => {
-//   try {
-//     ctx.database = await initDatabase(
-//       `${__dirname}/database/mongo`,
-//       process.env.DATABASE_URI
-//     );
-//   } catch (e) {
-//     console.error(e);
-//   }
-// });
-
 // Middlewares
-app.use(json());
-app.use(logger());
-app.use(bodyParser());
+app
+  .use(json())
+  .use(logger())
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods());
 
-app.use(router.routes()).use(router.allowedMethods());
-
-app.listen(3000, () => console.log('Server bound to port 3000'));
+app.listen(3000, () => console.log("Server bound to port 3000"));
